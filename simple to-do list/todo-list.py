e59b1b5039049bbe96f75704ed945157f65106c7
Ebenezer Acquah <ebenezeracquah000@gmail.com>
2026-07-03 21:34:31 +0000

    Complete to-do list

--- simple to-do list/todo-list.py
+++ simple to-do list/todo-list.py
@@ -38,3 +38,49 @@ def remove_task():
 
         else:
             print("Invalid task number.")
+
+
+while True:
+
+    print("\n===== TO-DO LIST =====")
+
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    try:
+
+        choice = int(input("Select an option: "))
+
+        if choice == 1:
+
+            add_task = input("Enter a task: ")
+
+            if add_task.strip() == "":
+                print("Task cannot be empty.")
+
+            else:
+                new_task(add_task)
+                print("Task added successfully.")
+
+        elif choice == 2:
+
+            view_tasks()
+
+        elif choice == 3:
+
+            remove_task()
+
+        elif choice == 4:
+
+            print("Thank you for using the To-Do List.")
+            break
+
+        else:
+
+            print("Enter a number from 1 to 4.")
+
+    except ValueError:
+
+        print("Sorry, that's an invalid input.")
