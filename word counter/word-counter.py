c8b5bf21cade03705efe6fcb66d34be5d33b6409
Ebenezer Acquah <ebenezeracquah000@gmail.com>
2026-07-07 22:59:12 +0000

    Complete Project

--- word counter/word-counter.py
+++ word counter/word-counter.py
@@ -1,14 +1,18 @@
 # ==========================
 # WORD COUNTER
 # ==========================
-def number_of_word (text):
+
+# Counts the number of words
+def number_of_word(text):
     words = len(text.split())
     return words
 
+# Counts characters excluding spaces
 def characters_without_whitespaces(text):
     words_without_whitespace = text.replace(" ", "")
-    return  len(words_without_whitespace)
+    return len(words_without_whitespace)
 
+# Counts all characters including spaces
 def characters_with_whitespaces(text):
     return len(text)
 
@@ -18,4 +22,3 @@ text = input("Enter some text:\n")
 print(number_of_word(text))
 print(characters_without_whitespaces(text))
 print(characters_with_whitespaces(text))
-
